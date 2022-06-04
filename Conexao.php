801ddf332cc0bc6ec4a912f3083e0c6d5e0fac0d
Mateus Lanes <lanesd2010@gmail.com>
2022-06-03 22:04:11 -0400

    ok

---
+++ Conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'root';
+$senha 	  = 'root';
+$banco    = 'banco';
+
+// FUNCOES DO SISTEMA DE CADASTRO ###########
+
+// $nomesite  = 'NOMEDASUAEMPRESA';
+// $urlmaster = 'https://seudelivery.tech'; // APENAS A URL PRINCIPAL SEM A BARRA NO FINAL ---- ----
+
+date_default_timezone_set('America/Sao_Paulo');
---
+++ funcoes/Conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'root';
+$senha 	  = 'root';
+$banco    = 'banco';
+
+// FUNCOES DO SISTEMA DE CADASTRO ###########
+
+$nomesite  = 'NOMEDASUAEMPRESA';
+$urlmaster = 'https://localhost'; // APENAS A URL PRINCIPAL SEM A BARRA NO FINAL ---- ----
+
+date_default_timezone_set('America/Sao_Paulo');
